8f752877469fedfb1bd88472fe7ad14fec74e43b
Charanjit Singh <charanjit.singh@trantorinc.com>
2019-06-17 18:50:42 +0530

    feat: insert :: LogMessage -> MessageTree -> MessageTree

--- cis194/src/LogAnalysis.hs
+++ cis194/src/LogAnalysis.hs
@@ -23,4 +23,8 @@ parse = mapFunc parseMessage . lines
         x:xs -> func x : mapFunc func xs
 
 insert :: LogMessage -> MessageTree -> MessageTree
-insert _ _ = Leaf
+insert (Unknown _) tree = tree
+insert msg Leaf         = Node Leaf msg Leaf
+insert m1@(LogMessage _ t1 _) (Node left m2@(LogMessage _ t2 _) right)
+  | t1 >= t2 = Node left m2 (insert m1 right)
+  | t1 < t2 = Node (insert m1 left) m2 right
