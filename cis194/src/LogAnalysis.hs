f009f606b032f8a6598b951932cca75af6060249
Charanjit Singh <charanjit.singh@trantorinc.com>
2019-06-17 17:55:03 +0530

    feat: parse String -> [LogAanalysis] with fmap

--- cis194/src/LogAnalysis.hs
+++ cis194/src/LogAnalysis.hs
@@ -1,5 +1,6 @@
 module LogAnalysis
   ( parseMessage
+  , parse
   ) where
 
 import           LogAnalysis.Internal
@@ -9,4 +10,8 @@ parseMessage str =
   case words str of
     "E":n:ts:ws -> LogMessage (Error $ read n) (read ts) (unwords ws)
     "I":ts:ws   -> LogMessage Info (read ts) (unwords ws)
+    "W":ts:ws   -> LogMessage Warning (read ts) (unwords ws)
     otherwise   -> Unknown str
+
+parse :: String -> [LogMessage]
+parse = fmap parseMessage . lines
