fe1524eb7a83834c96bb7f997c1d6aa3e7e678a4
Charanjit Singh <charanjit.singh@trantorinc.com>
2019-08-28 19:00:14 +0530

    feat: upgrade zero-bs-haskell cart to use Data.Map.Strict

--- zero-bs-haskell/src/Cart.hs
+++ zero-bs-haskell/src/Cart.hs
@@ -1,27 +1,45 @@
-module Cart (getCartHandler, createCartHanler) where
-import qualified Data.Aeson   as Aeson
-import           GHC.Generics (Generic)
-import qualified Zero.Server  as Server
+module Cart
+  ( Cart (..)
+  , getCartHandler
+  , createCartHanler
+  ) where
+import qualified Data.Aeson      as Aeson
+import           Data.List       (sortOn)
+import           Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import           GHC.Generics    (Generic)
+import qualified Zero.Server     as Server
+
 
 data Item = Item
   { model    :: String
   , quantity :: Int
   } deriving (Eq, Show, Generic, Aeson.ToJSON, Aeson.FromJSON)
-
-type Cart = [Item]
+data Cart =
+  Cart (Map String Item)
+  deriving (Eq, Show, Generic, Aeson.ToJSON, Aeson.FromJSON)
 
 getCartHandler :: Cart -> Server.Request -> (Cart, Server.Response)
-getCartHandler cart req = (cart, Server.jsonResponse cart)
+getCartHandler (Cart cart) req = (Cart cart, Server.jsonResponse $ items cart)
+  where
+    items = reverse . sortOn quantity . Map.elems
 
 createCartHanler :: Cart -> Server.Request -> (Cart, Server.Response)
-createCartHanler cart req = (newCart, Server.stringResponse msg)
+createCartHanler (Cart cart) req = (Cart newCart, Server.stringResponse msg)
   where
     item :: Either String Item
     item = Server.decodeJson $ Server.requestBody req
     newCart =
       case item of
-        Left _     -> []
-        Right item -> cart ++ [item]
+        Left _ -> Map.empty
+        Right item ->
+          case Map.lookup (model item) cart of
+            Just exItem ->
+              Map.insert
+                (model item)
+                (exItem {quantity = (quantity exItem) + (quantity item)})
+                cart
+            Nothing -> Map.insert (model item) item cart
     msg =
       case item of
         Left err   -> "Error while adding item" <> err
--- zero-bs-haskell/src/Lib.hs
+++ zero-bs-haskell/src/Lib.hs
@@ -1,13 +1,73 @@
 module Lib
-    ( run
-    ) where
+  ( run
+  ) where
 
-import qualified Data.Aeson   as Aeson
-import           GHC.Generics (Generic)
-import qualified Zero.Server  as Server
+import           Cart
+import qualified Data.Aeson      as Aeson
+import qualified Data.Map.Strict as Map
+import           GHC.Generics    (Generic)
+import qualified Zero.Server     as Server
 
 helloHandler :: Server.Request -> Server.Response
 helloHandler _ = Server.stringResponse "hello"
 
+echoHandler :: Server.Request -> Server.Response
+echoHandler = Server.stringResponse . Server.requestBody
+
+caseMatchHandler :: Server.Request -> Server.Response
+caseMatchHandler req =
+  case Server.requestBody req of
+    "1"       -> Server.stringResponse "one"
+    "2"       -> Server.stringResponse "two"
+    "3"       -> Server.stringResponse "three"
+    otherwise -> Server.stringResponse "What am I, a mathematician?"
+
+stringManipHandler :: Server.Request -> Server.Response
+stringManipHandler req = Server.stringResponse str_
+  where
+    str = Server.requestBody req
+    match = "I'm positive"
+    replace = "I think"
+    isMatch = take (length match) str == match
+    str_
+      | isMatch = replace <> drop (length match) str
+      | otherwise = str
+
+data OnOffState
+  = On
+  | Off
+  deriving (Eq, Show)
+
+type CounterState = Int
+
+onOffHandler :: OnOffState -> Server.Request -> (OnOffState, Server.Response)
+onOffHandler On req  = (Off, Server.stringResponse (show Off))
+onOffHandler Off req = (On, Server.stringResponse (show On))
+
+increaseHandler :: CounterState -> Server.Request -> (CounterState, Server.Response)
+increaseHandler n req = (n + 1, Server.stringResponse (show n))
+
+countHandler :: CounterState -> Server.Request -> (CounterState, Server.Response)
+countHandler n req = (n, Server.stringResponse (show n))
+
 run :: IO ()
-run = Server.startServer [Server.simpleHandler Server.GET "/hello" helloHandler]
+run =
+  Server.startServer
+    [ Server.simpleHandler Server.GET "/hello" helloHandler
+    , Server.simpleHandler Server.POST "/echo" echoHandler
+    , Server.simpleHandler Server.POST "/case" caseMatchHandler
+    , Server.simpleHandler Server.POST "/string-manipulation" stringManipHandler
+    , Server.handlersWithState
+        Off
+        [Server.statefulHandler Server.POST "/onoff-switch" onOffHandler]
+    , Server.handlersWithState
+        0
+        [ Server.statefulHandler Server.POST "/increase" increaseHandler
+        , Server.statefulHandler Server.GET "/current-count" countHandler
+        ]
+    , Server.handlersWithState
+        (Cart Map.empty)
+        [ Server.statefulHandler Server.POST "/cart" createCartHanler
+        , Server.statefulHandler Server.GET "/cart" getCartHandler
+        ]
+    ]
