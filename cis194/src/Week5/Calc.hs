8f53c268a77ed33bb5fa3b8d35ad31a6dd30b31d
Charanjit Singh <charanjit.singh@trantorinc.com>
2020-07-09 14:08:35 +0530

    feat: add Week5 exercise 2

--- cis194/src/Week5/Calc.hs
+++ cis194/src/Week5/Calc.hs
@@ -1,9 +1,13 @@
 module Week5.Calc where
 
 import           Week5.ExprT
+import           Week5.Parser
 
 eval :: ExprT -> Integer
 eval x = case x of
   Lit n   -> n
   Add a b -> (eval a) + (eval b)
   Mul a b -> (eval a) * (eval b)
+
+evalStr :: String -> Maybe Integer
+evalStr = fmap eval . parseExp Lit Add Mul
--- cis194/test/Week5Spec.hs
+++ cis194/test/Week5Spec.hs
@@ -12,3 +12,8 @@ spec = do
   describe "eval" $ do
     it "should return correct result" $ do
       eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4)) `shouldBe` 20
+
+  describe "evalStr" $ do
+    it "should return correct result" $ do
+      evalStr "2+3*5" `shouldBe` Just 17
+      evalStr "2+3*" `shouldBe` Nothing
