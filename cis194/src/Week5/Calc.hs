7a4dc4fda3afa6f2c21548c1c19de11c3de05e9d
Charanjit Singh <charanjit.singh@trantorinc.com>
2020-07-12 08:14:42 +0530

    feat: add week5 exercise 4

--- cis194/src/Week5/Calc.hs
+++ cis194/src/Week5/Calc.hs
@@ -2,6 +2,7 @@ module Week5.Calc where
 
 import           Week5.ExprT
 import           Week5.Parser
+import qualified Week5.StackVM as VM
 
 eval :: ExprT -> Integer
 eval x = case x of
@@ -21,3 +22,42 @@ instance Expr ExprT where
   lit x = Lit x
   add a b = Add a b
   mul a b = Mul a b
+
+instance Expr Integer where
+  lit x = x
+  add a b = a + b
+  mul a b = a * b
+
+instance Expr Bool where
+  lit a
+    | a <= 0 = False
+    | otherwise = True
+  add a b = a || b
+  mul a b = a && b
+
+newtype MinMax = MinMax Integer deriving (Eq, Show, Ord)
+
+instance Expr MinMax where
+  lit = MinMax
+  add = max
+  mul = min
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show, Ord)
+
+mod7 a
+  | a < 0 = 0
+  | a < 7 = a
+  | otherwise = mod7 (a `mod` 7)
+
+instance Expr Mod7 where
+  lit a = Mod7 $ mod7 a
+  add (Mod7 a) (Mod7 b) = Mod7 $  mod7 (a + b)
+  mul (Mod7 a) (Mod7 b) = Mod7 $  mod7 (a * b)
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger  = testExp :: Maybe Integer
+testBool     = testExp :: Maybe Bool
+testMM       = testExp :: Maybe MinMax
+testSat      = testExp :: Maybe Mod7
--- cis194/test/Week5Spec.hs
+++ cis194/test/Week5Spec.hs
@@ -17,3 +17,16 @@ spec = do
     it "should return correct result" $ do
       evalStr "2+3*5" `shouldBe` Just 17
       evalStr "2+3*" `shouldBe` Nothing
+
+  describe "Expr" $ do
+    it "should create valid instance for integer" $ do
+      testInteger `shouldBe` Just (-7)
+
+    it "should create valid instance for Bool" $ do
+      testBool `shouldBe` Just True
+
+    it "should create valid instance for MinMax" $ do
+      testMM `shouldBe` Just (MinMax 5)
+
+    it "should create valid instance for Mod7" $ do
+      testSat `shouldBe` Just (Mod7 5)
